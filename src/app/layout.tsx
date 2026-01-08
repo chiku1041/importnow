a14962c782c576bcb9579577af8b38a5ecd57450
import now <dinesh.9ze@gmail.com>
2026-01-09 00:07:37 +0530

    Add Google Analytics tag (G-VE014C8LBS)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter, Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import { Header } from "@/components/layout/Header";
 import { Footer } from "@/components/layout/Footer";
@@ -86,6 +87,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-VE014C8LBS"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-VE014C8LBS');
+          `}
+        </Script>
+      </head>
       <body
         className={`${inter.variable} ${geistSans.variable} ${geistMono.variable} antialiased font-sans`}
       >
--- src/app/oneprice/layout.tsx
+++ src/app/oneprice/layout.tsx
@@ -37,3 +37,4 @@ export default function OnePriceLayout({
   return <>{children}</>;
 }
 
+
